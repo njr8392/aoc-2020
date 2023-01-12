56ba45bbd01a0649297748f1530d340238431031
Nicholas Rodgers <nrodgers88@gmail.com>
2023-01-11 22:46:48 -0500

    day part 2

--- 2022/day6/main.go
+++ 2022/day6/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	data := util.ReadInput("input.txt")
 	fmt.Println(p1(data))
+	fmt.Println(p2(data))
 }
 
 //find first instance of 4 unique characters
@@ -15,7 +16,6 @@ func p1(b []byte) int {
 	buf := make([]byte, 4)
 	for i := range b{
 		copy(buf, b[i:i+4])
-		fmt.Println(string(buf))
 		m := make(map[byte]bool)
 		for _, char := range buf {
 			m[char] = true
@@ -23,6 +23,22 @@ func p1(b []byte) int {
 
 		if len(m) == 4 {
 			return i + 4
+		}
+	}
+	return 0
+}
+
+func p2(b []byte) int {
+	buf := make([]byte, 14)
+	for i := range b{
+		copy(buf, b[i:i+14])
+		m := make(map[byte]bool)
+		for _, char := range buf {
+			m[char] = true
+		}
+
+		if len(m) == len(buf) {
+			return i + 14
 		}
 	}
 	return 0
